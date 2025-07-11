23519ca7706dd1df36ae3a50cd03ee3006e5b724
Muhammad-Muhib <muhib@lumensoft.biz>
2025-07-12 00:06:13 +0500

    Bug Fixes

--- src/Controls/Input/InputNumber.jsx
+++ src/Controls/Input/InputNumber.jsx
@@ -1,12 +1,16 @@
 export default function InputNumber({customClass,label,name,customInputClass,inputVal,setInputVal,important,disable}) {
+  const handleChange = (e) => {
+    let val = e.target.value;
+    // Allow only digits and one optional decimal
+    if (/^[0-9]*\.?[0-9]*$/.test(val)) {
+      setInputVal(val);
+    }
+  };
   return (
     <div className={`form-group SearchField ${customClass}`}>
         <div className="inputContainer">
                         <div className="inputLabel" >{label}</div>
-                        {
-                          name != null? <input  name={`${name}`} id="" type="text" onClick={(e)=>e.target.select()} className={`form-control number_input candela_input ${customInputClass}`} value={inputVal} onChange={(e)=>{setInputVal(e)}} style={important == "true" ? {backgroundColor:'#FFFFE8'} : {}} disabled={disable || false} /> : 
-                          <input  id="" type="text" onClick={(e)=>e.target.select()} className={`form-control number_input candela_input ${customInputClass}`} value={inputVal} onChange={(e)=>{setInputVal(e.target.value)}} style={important == "true" ? {backgroundColor:'#FFFFE8'} : {}} disabled={disable || false} />
-                        }
+                        <input name={`${name}`} id="" type="text" onClick={(e)=>e.target.select()} className={`form-control number_input candela_input ${customInputClass}`} value={inputVal} onChange={(e)=>{handleChange(e)}} style={important == "true" ? {backgroundColor:'#FFFFE8'} : {}} disabled={disable || false} />
         </div>
     </div>
   )
