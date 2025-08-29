9c5ce8c629f405ae2a309f093d0fb340839e5472
Muhammad-Muhib <muhib@lumensoft.biz>
2025-08-29 14:29:33 +0500

    bug fixes

--- src/Controls/Tooltip/CustomTooltip.jsx
+++ src/Controls/Tooltip/CustomTooltip.jsx
@@ -2,37 +2,34 @@ import { styled } from '@mui/material/styles';
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 
-export default function CustomTooltip({children,title = "",body = "",placement = "bottom"}) {
-    const HtmlTooltip = styled(({ className, ...props }) => (
-  <Tooltip {...props} classes={{ popper: className }} arrow placement={placement} />
-))(({ theme }) => ({
-  [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: 'white',
-    color: 'rgba(0, 0, 0, 0.87)',
-    maxWidth: 220,
-    fontSize: theme.typography.pxToRem(12),
-    border:'1px solid rgba(0,0,0,0.4)',
-    boxShadow:'2px 2px 2px rgba(0,0,0,0.4)'
-  },
-}));
+export default function CustomTooltip({ children, title = "", body = "", placement = "bottom" }) {
+  const HtmlTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} arrow placement={placement} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: 'white',
+      color: 'rgba(0, 0, 0, 0.87)',
+      maxWidth: 220,
+      fontSize: theme.typography.pxToRem(12),
+      border: '1px solid rgba(0,0,0,0.4)',
+      boxShadow: '2px 2px 2px rgba(0,0,0,0.4)',
+    },
+  }));
+
+  if (title.trim() === "" && body.trim() === "") {
+    return <>{children}</>;
+  }
+
   return (
-    <>
-    {
-      title.trim() != "" || body.trim() != "" ?     <HtmlTooltip 
-        title={
-          <>
-            {
-                title.trim() != "" && <Typography color="inherit">{title}</Typography>
-            }
-            {
-                body.trim() != "" && <span>{body}</span>
-            }            
-          </>
-        }
-      >
-         {children}
-      </HtmlTooltip> : <></>
-    }
-    </>    
-  )
+    <HtmlTooltip
+      title={
+        <>
+          {title.trim() !== "" && <Typography color="inherit">{title}</Typography>}
+          {body.trim() !== "" && <span>{body}</span>}
+        </>
+      }
+    >
+      {children}
+    </HtmlTooltip>
+  );
 }
