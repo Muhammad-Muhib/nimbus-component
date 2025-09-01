95976ffc62bbf863a7160886f91b1bb9d435817d
Muhammad-Muhib <muhib@lumensoft.biz>
2025-09-01 10:07:14 +0500

    item code fixes

--- src/Controls/Input/ItemCodeInput.jsx
+++ src/Controls/Input/ItemCodeInput.jsx
@@ -5,23 +5,38 @@ export default function ItemCodeInput({
   setInputVal,
   important,
   id,
-  maxLength
+  maxLength,
+  inputRef,
+  handleBlur
 }) {
+  const internalRef = useRef(null);
+  useEffect(() => {
+    if (inputRef) {
+      inputRef.current = internalRef.current;
+    }
+  }, [inputRef]);
+  const handleInputChange = (e) => {
+    let value = e.target.value;    
+    value = value.replace(/[^a-zA-Z\s]/g, "");
+    setInputVal(value);
+  };
   return (
     <div className={`form-group SearchField ${customClass}`}>
       <div className="inputContainer">
         <div className="inputLabel">{label}</div>
         <input
+          ref={internalRef}
           onClick={(e) => e.target.select()}
           id={`${id}`}
           type="text"
           className="form-control item_code_input candela_input"
           value={inputVal}
           onChange={(e) => {
-            setInputVal(e.target.value);
+            handleInputChange(e);
           }}
           style={important == "true" ? { backgroundColor: "#FFFFE8" } : {}}
           maxLength={`${maxLength}`}
+          onBlur={handleBlur}
         />
       </div>
     </div>
