224638b7de31430c34e24f0c9080d20cd7fcd5fd
Muhammad-Muhib <muhib@lumensoft.biz>
2025-09-02 11:43:49 +0500

    bug fixes

--- src/Controls/RightDrawer/RightDrawer.jsx
+++ src/Controls/RightDrawer/RightDrawer.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
 import OptionSelection from "../DropDown/OptionSelection";
 
@@ -7,32 +7,40 @@ const RightDrawer = ({
   onClose,
   videoModel = [],
   articleModel = [],
-  setIsOpen,  
+  setIsOpen,
 }) => {
-    const [videoSlice,setVideoSlice] = useState([])
-    const languageOptions = [
-        {value:"1",label:"English"},
-        {value:"2",label:"Urdu"}
-    ]
-    const [selectedLanguage,setSelectedLanguage] = useState({value:"1",label:"English"})
-    
-    useEffect(() => {
-            const handleKeyDown = (e) => {
-                if (e.key == "Escape") {
-                    setIsOpen(false);
-                }
-            };
-            document.addEventListener("keydown", handleKeyDown);
-    
-            return () => {
-                document.removeEventListener("keydown", handleKeyDown);
-            };
-        }, [isOpen]);
-        useEffect(()=>{
-            if(videoModel.length > 0){
-                setVideoSlice(videoModel.filter((item)=>item.language.toLowerCase() == selectedLanguage.label.toLowerCase()))
-            }
-        },[videoModel,selectedLanguage])     
+  const [videoSlice, setVideoSlice] = useState([]);
+  const languageOptions = [
+    { value: "1", label: "English" },
+    { value: "2", label: "Urdu" },
+  ];
+  const [selectedLanguage, setSelectedLanguage] = useState({
+    value: "1",
+    label: "English",
+  });
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key == "Escape") {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen]);
+  useEffect(() => {
+    if (videoModel.length > 0) {
+      setVideoSlice(
+        videoModel.filter(
+          (item) =>
+            item.language.toLowerCase() == selectedLanguage.label.toLowerCase()
+        )
+      );
+    }
+  }, [videoModel, selectedLanguage]);
   return (
     <>
       {/* Overlay */}
@@ -56,47 +64,49 @@ const RightDrawer = ({
 
         {/* Drawer Content */}
         <div className="drawer-content">
-            {
-                videoModel.length > 0 && <div className="videosContainer">
-            <h2 className="videoHeader">Videos</h2>
-            <OptionSelection
-            label={"Language"}
-            options={languageOptions}
-            selectedOption={selectedLanguage}
-            setSelectedOption={setSelectedLanguage}
-             />
-            <div className="videoCardContainer">
-              {videoSlice.map((item, index) => {
-                return (
-                  <a
-                    href={`${item.youTubeLink}`}
-                    className="videoCard"
-                    key={index}
-                  >
-                    <img src={`${item.youTubePic}`} className="youTubeImg" />
-                    <div className="youtubeText">{item.description}</div>
-                  </a>
-                );
-              })}
-            </div>
-          </div>
-            }
-            {
-                articleModel.length > 0 && <div className="articleContainer">
-            <h2 className="videoHeader">Articles</h2>
-            <div className="artcleCardContainer">
-              {articleModel.map((item, index) => {
-                return (
-                  <div key={index}>
-                    <a href={`${item.articleLink}`} className="articleText">
-                      {item.articleText}
+          {videoModel.length > 0 && (
+            <div className="videosContainer">
+              <h2 className="videoHeader">Videos</h2>
+              <div className="videoLanguageDropDownContainer">
+                <OptionSelection
+                  label={"Language"}
+                  options={languageOptions}
+                  selectedOption={selectedLanguage}
+                  setSelectedOption={setSelectedLanguage}
+                />
+              </div>
+              <div className="videoCardContainer">
+                {videoSlice.map((item, index) => {
+                  return (
+                    <a
+                      href={`${item.youTubeLink}`}
+                      className="videoCard"
+                      key={index}
+                    >
+                      <img src={`${item.youTubePic}`} className="youTubeImg" />
+                      <div className="youtubeText">{item.description}</div>
                     </a>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
-          </div>
-            }
+          )}
+          {articleModel.length > 0 && (
+            <div className="articleContainer">
+              <h2 className="videoHeader">Articles</h2>
+              <div className="artcleCardContainer">
+                {articleModel.map((item, index) => {
+                  return (
+                    <div key={index}>
+                      <a href={`${item.articleLink}`} className="articleText">
+                        {item.articleText}
+                      </a>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
