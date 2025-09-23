660dba4af6bfc799157b8c862a3d79d8531d499f
Muhammad-Muhib <muhib@lumensoft.biz>
2025-09-23 14:05:30 +0500

    bug fixes 2.0

--- src/Controls/Popup/CustomInformationPopup.jsx
+++ src/Controls/Popup/CustomInformationPopup.jsx
@@ -1,51 +1,72 @@
-import React, { useState,useEffect,useRef } from 'react'
+import React, { useState, useEffect, useRef } from "react";
 
-export default function CustomInformationPopup({setShowModal,onConfirm,modalBody,modalTitle,headerColor = "",btnColor = ""}) {
-    const [addBgColor,setAddBgColor] = useState(true)
-    const okRef = useRef();
-    useEffect(() => {
-      okRef.current.focus()
-        const handleKeyDown = (e) => {
-          if (e.key == "Escape") {
-            setShowModal(false);
-          }
-        };    
-        document.addEventListener("keydown", handleKeyDown);    
-        return () => {
-          document.removeEventListener("keydown", handleKeyDown);
-        };
-      }, []);
+export default function CustomInformationPopup({
+  setShowModal,
+  onConfirm,
+  modalBody,
+  modalTitle,
+  headerColor = "",
+  btnColor = "",
+}) {
+  const [addBgColor, setAddBgColor] = useState(true);
+  const okRef = useRef();
+  useEffect(() => {
+    okRef.current.focus();
+    const handleKeyDown = (e) => {
+      if (e.key == "Escape") {
+        setShowModal(false);
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
-    <div
-            className="modal fade show confirmationModal"
-            tabIndex="-1"
+    <div className="modal fade show confirmationModal" tabIndex="-1">
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div
+            className="custom-modal-header"
+            style={{
+              backgroundColor: headerColor == "" ? "#3f4d54" : headerColor,
+            }}
           >
-            <div className="modal-dialog modal-dialog-centered">
-              <div className="modal-content">
-                <div className="custom-modal-header" style={{backgroundColor:headerColor == "" ? "#3f4d54" : headerColor }}>
-                  <h5 className="modal-title">{modalTitle}</h5>
-                </div>
-                <div className="modal-body">
-                  <div className="displayGroupName">{modalBody}</div>              
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary custom-confirmationbtn"
-                    style={{backgroundColor:btnColor == "" ? "black" : btnColor,borderColor:btnColor == "" ? "black" : btnColor}}
-                    id='okBtn'                    
-                    onMouseEnter={()=>setAddBgColor(true)}
-                    onClick={()=>{
-                        setShowModal(false)
-                        onConfirm()
-                    }}
-                    ref={okRef}
-                  >
-                    OK
-                  </button>
-                </div>
-              </div>
+            <h5 className="modal-title custom-modal-title">{modalTitle}</h5>
+            <div className="closeBtnContainer">
+              <button
+                type="button"
+                onClick={() => setShowModal(false)}
+                className="btn-close modalCloseBtn"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
           </div>
-  )
+          <div className="modal-body">
+            <div className="displayGroupName">{modalBody}</div>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary custom-confirmationbtn"
+              style={{
+                backgroundColor: btnColor == "" ? "black" : btnColor,
+                borderColor: btnColor == "" ? "black" : btnColor,
+              }}
+              id="okBtn"
+              onMouseEnter={() => setAddBgColor(true)}
+              onClick={() => {
+                setShowModal(false);
+                onConfirm();
+              }}
+              ref={okRef}
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
